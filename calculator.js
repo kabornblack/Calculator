8d3b625ebfb6483f06f59c3f3c2294055be3f33c
kabornblack <shaibu.kabiru@email.com>
2023-03-04 14:06:53 +0200

    Responding to request with HTMLfiles

--- calculator.js
+++ calculator.js
@@ -3,8 +3,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello world!")
-})
+  res.sendFile(__dirname + "/index.html");
+});
 
 
 app.listen(3000, function(req, res){
