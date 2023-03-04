44904bead9d8e84956253b06e6120d9389883cd0
kabornblack <shaibu.kabiru@email.com>
2023-03-04 17:44:31 +0200

    A simple calculator and BMI calculator WebApp

--- calculator.js
+++ calculator.js
@@ -9,11 +9,22 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
 app.post("/", function(req, res){
 var num1 = Number(req.body.num1);
 var num2 = Number(req.body.num2);
 var result = num1 + num2;
   res.send("The answer is " + result);
+});
+
+app.post("/bmicalculator", function(req, res){
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight/(height**2);
+    res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function(req, res){
